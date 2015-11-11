11dbf2aa0e0478148805eddd14f005e9bb1efcfb
Matthew Vaughn <vaughn@tacc.utexas.edu>
2015-11-11 05:32:28 -0600

    Minor update to cufflinks _common files to set R path correctly

--- dnasubway/cufflinks/2.1.1/lonestar4/_common_begin.sh
+++ dnasubway/cufflinks/2.1.1/lonestar4/_common_begin.sh
@@ -1,13 +1,20 @@
-# Common to all cufflinks apps
+# Functions common to all cufflinks apps
+#
 # Cannot use Agave macros in here
 
 # These are not in the Github repo as they are too large
 # but are implicitly stored on the storageSystem
 tar zxf ./bin.tgz
-tar zxf ./R-3.2.0.tgz
 export PATH="$PATH:$PWD/bin"
-export PATH="$PATH:$PWD/R-3.2.0/bin"
-export R_LOCAL=$PWD/R-3.2.0
+
+tar zxf ./R-3.2.0.tgz
+export PATH="$PATH:$PWD/R/bin"
+export R_LOCAL=$PWD/R
+
+# Directories
+export CWD=${PWD}
+export TMPDIR="$CWD/tmp"
+mkdir $TMPDIR
 
 # Should probably use the $NSLOTS environment variable to calculate thread info if possible
 THREADS=$(cat /proc/cpuinfo | grep processor | wc -l)
@@ -16,11 +23,6 @@ then
   THREADS=4
 fi
 export THREADS
-
-# Directories
-export CWD=${PWD}
-export TMPDIR="$CWD/tmp"
-mkdir $TMPDIR
 
 # Convenience functions
 echoerr() { echo -e "$@" 1>&2; }
--- dnasubway/cufflinks/2.1.1/lonestar4/_common_end.sh
+++ dnasubway/cufflinks/2.1.1/lonestar4/_common_end.sh
@@ -1,1 +1,1 @@
-rm -rf bin R-3.2.0 tmp bin.tgz R-3.2.0.tgz
+rm -rf bin R-3.2.0 R tmp bin.tgz R-3.2.0.tgz
